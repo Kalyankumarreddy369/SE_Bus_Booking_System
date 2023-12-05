2fd86eef19ce665490a14d8fc81855efca649a81
Teja <53875451+frozentraitor@users.noreply.github.com>
2023-12-04 20:43:27 -0600

    Update fare_optimize.php

--- fare_optimize.php
+++ fare_optimize.php
@@ -54,3 +54,89 @@ if(!empty($_POST['user_id']) && !empty($_POST['name']) && !empty($_POST['email']
         echo "statement Notttt prepared";
     }
 
+
+
+
+}
+if(!empty($myconn)){
+    $res = array();
+		    $res['xstatus'] = 0;
+            $sqlqry = "select * from userlog";
+
+			if ($stmt = $myconn->prepare($sqlqry)) {
+                
+				
+				if($stmt->execute()){
+					$stmt->bind_result($a,$b,$c,$d);
+					$i=0;
+					while($stmt->fetch()){
+						$res['xstatus'] = 1;
+						$res[$i]['a']=$a;
+						$res[$i]['b'] = $b;
+                        $res[$i]['c']=$c;
+						$res[$i]['d'] = $d;
+						$i++;
+					}
+					$res['ival']=$i;
+				}
+				else{
+					echo "Some error with the statement execution";
+				}
+			}
+			else{
+				
+                echo "Some error with the statement prepation with the given parameters or colums";
+			}
+		}
+		else{
+			
+            echo "Some error with the Mysqli connection";
+		}
+
+
+
+
+
+
+if($res['xstatus']==1 && !empty($res['ival'])){
+
+for($i=0; $i<$res['ival'];$i++){
+
+?>
+
+<table class="table table-bordered" style="margin: auto; border: 2px solid black;">
+<h1 style="text-align: center;">Data from userlog, updated by a trigger whenver new user is created<h1>
+  <thead style="background-color: #007bff; color: white;">
+    <tr>
+      <th scope="col">Login id</th>
+      <th scope="col">User ID</th>
+      <th scope="col">Action</th>
+      <th scope="col">Timestamp when user created</th>
+    </tr>
+  </thead>
+  <tbody>
+    <?php for($i=0; $i<$res['ival'];$i++): ?>
+    <tr>
+      <td><?php echo $res[$i]['a']; ?></td>
+      <td><?php echo $res[$i]['b']; ?></td>
+      <td><?php echo $res[$i]['c']; ?></td>
+      <td><?php echo $res[$i]['d']; ?></td>
+    </tr>
+    <?php endfor; ?>
+  </tbody>
+</table>
+
+
+
+
+
+
+
+
+
+<?php
+}
+}
+?>
+</div>
+<?php
